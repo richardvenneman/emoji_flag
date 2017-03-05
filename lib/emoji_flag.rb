933d75a701aa5551448c838ed3f93cd24dde9dee
Richard Venneman <richardvenneman@me.com>
2017-03-05 13:18:23 +0100

    Add initial implementation

--- lib/emoji_flag.rb
+++ lib/emoji_flag.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 module EmojiFlag
+  OFFSET = 127_397
+  COUNTRY_CODE_RE = /^[A-Z]{2}$/
+
   def self.new(input)
-    true
+    code = input.to_s.upcase
+    return '' unless COUNTRY_CODE_RE.match(code)
+
+    code.codepoints.collect { |codepoint| codepoint + OFFSET }.pack('U*')
   end
 end
--- test/lib/emoji_flag_test.rb
+++ test/lib/emoji_flag_test.rb
@@ -4,6 +4,20 @@ require 'test_helper'
 
 class EmojiFlagTest < Minitest::Test
   def test_constructor
-    assert_equal true, EmojiFlag.new('test')
+    assert_equal true, EmojiFlag.respond_to?(:new)
+  end
+
+  def test_invalid_code_returns_empty_string
+    assert_equal '', EmojiFlag.new('')
+    assert_equal '', EmojiFlag.new('derp')
+  end
+
+  def test_valid_code_returns_emoji
+    assert_equal '🇳🇱', EmojiFlag.new('nl')
+    assert_equal '🇸🇪', EmojiFlag.new('SE')
+  end
+
+  def test_symbol_code_returns_emoji
+    assert_equal '🇩🇪', EmojiFlag.new(:de)
   end
 end
