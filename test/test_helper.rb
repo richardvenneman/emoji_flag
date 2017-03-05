b640bd25c89fd09f6c80a9ed72da14e0611bdac1
Richard Venneman <richardvenneman@me.com>
2017-03-05 14:44:24 +0100

    Update SimpleCov init

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,6 @@
-# Coverage
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-elsif ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start do
-    add_group 'Libraries', 'lib'
-  end
+require 'simplecov'
+SimpleCov.start do
+  add_group 'Libraries', 'lib'
 end
 
 # Source files
